33c221f146f20bf67a45e458baa4362e67422ee4
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2024-05-15 22:15:44 -0700

    First Move Complete

--- main.js
+++ main.js
@@ -2,9 +2,31 @@ var board = [['---', 5, 4, 3, 2, 1],
 ['---'],
 ['---']];
 
+
 var startingBoard = board.map(function (peg) {
     board.join('/n')
     return peg.join('');
 });
 
-console.log(startingBoard);
+
+
+var firstMove = function (fromPeg, toPeg) {
+  if (fromPeg == 1 && toPeg == 2) {
+    if (board[0] > board[1]) {
+      board[0].pop();
+      board[1].push(1);
+      
+      return 'That move was successful, the board is now: ' + board;
+    } else return 'That move was unsuccessful, the board remains: ' + board;
+  };
+  if (fromPeg == 1 && toPeg == 3) {
+    if (board[0] > board[2]) {
+      board[0].pop();
+      board[2].push(1);
+      
+      return 'That move was successful, the board is now: ' + board;
+    } else return 'That move was unsuccessful, the board remains: ' + board;
+  };
+}
+
+ console.log(firstMove(1, 3));
