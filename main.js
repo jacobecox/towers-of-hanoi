29ef33d4efc84c31729937d4a9c822b7116e32f3
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2024-05-21 14:39:46 -0700

    game near completion

--- main.js
+++ main.js
@@ -1,105 +1,68 @@
 // GAME SETUP
 
-var board = [['---', 5, 4, 3, 2, 1],
-['---'],
-['---']];
+var board = [[5, 4, 3, 2, 1],
+[],
+[]];
 
-// I got the board to project right in startingBoard but I could not call any array helper methods on it since it created a new array using .map. Not sure how to work around that but the rest of the game just printed board rather than startingBoard.
-var startingBoard = board.map(function (peg) {
+var currentBoard = board.map(function (peg) {
     board.join('/n')
     return peg.join('');
 });
 
-var winnerBoard1 = [['---'],
-['---'],
-['---', 5, 4, 3, 2, 1]];
+function printBoard(){
+  console.log({board});
+}
 
-var winnerBoard2 = [['---'],
-['---', 5, 4, 3, 2, 1],
-['---']];
+console.log(board)
+
+var winnerBoard1 = [[],
+[],
+[ 5, 4, 3, 2, 1]];
+
+var winnerBoard2 = [[],
+[5, 4, 3, 2, 1],
+[]];
+
 
 
 // GAME PLAY
 
-// I feel like I could've made a move that worked for every move after but I couldn't figure out how to write .push() to fit any given number instead of having to add an explicit number in the argument.
-var firstMove = function (fromPeg, toPeg) {
-  if (fromPeg == 1 && toPeg == 2) {
-    if (board[0] > board[1]) {
-      board[0].pop();
-      board[1].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-  if (fromPeg == 1 && toPeg == 3) {
-    if (board[0] > board[2]) {
-      board[0].pop();
-      board[2].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-}
+function move(from, to){
+// compare last peg (from) and last peg (to)
+  var lastFrom = board[from].at(-1)
+  var lastTo = board[to].at(-1)  
 
-// nextMove worked well except I cannot think of a way to show the value of board[0], board[1], or board[2] being greater than each other since the conditional I wrote checks the index length and not the value of the number.
-var nextMove = function (fromPeg, toPeg) {
-  if (fromPeg == 1 && toPeg == 2) {
-    if (board[0] > board[1]) {
-      board[0].pop();
-      board[1].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-  if (fromPeg == 1 && toPeg == 3) {
-    if (board[0] > board[2]) {
-      board[0].pop();
-      board[2].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-  if (fromPeg == 2 && toPeg == 1) {
-    if (board[1] > board[0]) {
-      board[1].pop();
-      board[0].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-  if (fromPeg == 3 && toPeg == 1) {
-    if (board[2] > board[0]) {
-      board[2].pop();
-      board[0].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-  if (fromPeg == 2 && toPeg == 3) {
-    if (board[1] > board[2]) {
-      board[1].pop();
-      board[2].push(1);
-      
-      return 'That move was successful, the board is now: ' + board;
-    } else return 'That move was unsuccessful, the board remains: ' + board;
-  };
-}
+  console.log(lastFrom, lastTo)
 
+// logic checks
+if (from == to) {
+  return 'Invalid move, try again.'
+} else if (from < 0 || from > 3) {
+  return 'Invalid move, try again.'
+} else if (to < 0 || to > 3) {
+  return 'Invalid move, try again.'
+} else if (!lastFrom) {
+  return 'Invalid move, try again'
+} else if (!lastTo){
+  board[to].push(board[from].pop());
+  } else if (lastFrom > lastTo) {
+  return 'The peg you move must be smaller than the peg beneath, try again.';
+} else {
+board[to].push(board[from].pop());
+printBoard();
+checkWinner();
+}};
+
+move(2, 2);
 
 
 // GAME RULES
 
-// I wasn't too sure how else to compare arrays using == or === except by converting them to strings. I understand that would not work if they had objects or functions in the arrays but for this case it worked.
-var checkWinner = function() {
+function checkWinner() {
   if (JSON.stringify(board) === JSON.stringify(winnerBoard1)) {
-    board = [['---', 5, 4, 3, 2, 1],
-    ['---'],
-    ['---']];
-  console.log('Congratulations! You Win! Board is now: ' + board);
+    board === board
+  console.log('Congratulations! You win! Play again? ' + board);
 } else if (JSON.stringify(board) === JSON.stringify(winnerBoard2)) {
-  board = [['---', 5, 4, 3, 2, 1],
-  ['---'],
-  ['---']];
-console.log('Congratulations! You Win! Board is now: ' + board);
+console.log('Congratulations! You win! Play again? ' + board);
 } else return;
 };
